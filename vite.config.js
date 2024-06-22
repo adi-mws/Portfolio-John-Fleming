cd270af856d9040662ece8ecaeb59f473333b60b
Aditya Raj <aditya040mth@gmail.com>
2024-06-22 10:41:38 +0530

    Configuration Settings Updated

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Customize output directory name if needed
     base: '/Portfolio-John-Fleming/',
-    assetsDir: './assets'
   },
 })
 
